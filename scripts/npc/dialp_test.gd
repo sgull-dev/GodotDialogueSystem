85734339c5e8a0813b5ec773bc81442566cf72a8
sgull-dev <122727788+sgull-dev@users.noreply.github.com>
2023-08-23 10:45:10 +0300

    background + background test dialogue

--- scripts/npc/dialp_test.gd
+++ scripts/npc/dialp_test.gd
@@ -3,10 +3,16 @@ extends Node3D
 #Here is a sample script showing how a DialoguePicker (dialp_) script should look like
 #two functions to assess incoming requests and feed correct dialogue paths to the system
 
+var test_index = 1
+
 @onready var player = $"../DialoguePlayer"
 
 func pick_dialogue() -> String:
-	return "res://assets/dialogue/test_1.json"
+	if test_index == 1:
+		test_index += 1
+		return "res://assets/dialogue/test_1.json"
+	else:
+		return "res://assets/dialogue/cell_1.json"
 
 func receive_choice(string):
 	match string:
